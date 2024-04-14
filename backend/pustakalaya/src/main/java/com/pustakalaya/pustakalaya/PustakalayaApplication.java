11b12e74c505a316d883b22781cbbcc5fe0fbb0b
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-04-14 19:49:06 +0545

    Pustakalaya Backend set up

---
+++ backend/pustakalaya/src/main/java/com/pustakalaya/pustakalaya/PustakalayaApplication.java
@@ -0,0 +1,13 @@
+package com.pustakalaya.pustakalaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PustakalayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PustakalayaApplication.class, args);
+	}
+
+}
