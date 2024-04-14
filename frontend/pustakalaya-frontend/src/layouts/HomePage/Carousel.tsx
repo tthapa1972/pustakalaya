9bc70fa9c44698b6002ba9f898a12f9d30a79434
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-04-14 16:04:57 +0545

    added Return Book Component

--- frontend/pustakalaya-frontend/src/layouts/HomePage/Carousel.tsx
+++ frontend/pustakalaya-frontend/src/layouts/HomePage/Carousel.tsx
@@ -1,3 +1,5 @@
+import { ReturnBook } from "./ReturnBook";
+
 export const Carousel = () => {
     return (
         <div className="container mt-5" style={{ height: 550 }}>
@@ -9,45 +11,24 @@ export const Carousel = () => {
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                         <div className="row d-flex justify-content-center align-items-center">
-                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div className="text-center">
-                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='151' height='233' alt="book" />
-                                    <h6 className="mt-2">Book</h6>
-                                    <p>Luv2Code</p>
-                                    <a className="btn main-color text-white" href="#">Reserve</a>
-                                </div>
-
-                            </div>
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
                         </div>
-
                     </div>
                     <div className="carousel-item">
                         <div className="row d-flex justify-content-center align-items-center">
-                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div className="text-center">
-                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='151' height='233' alt="book" />
-                                    <h6 className="mt-2">Book</h6>
-                                    <p>Luv2Code</p>
-                                    <a className="btn main-color text-white" href="#">Reserve</a>
-                                </div>
-
-                            </div>
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
                         </div>
-
                     </div>
                     <div className="carousel-item">
                         <div className="row d-flex justify-content-center align-items-center">
-                            <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                                <div className="text-center">
-                                    <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='151' height='233' alt="book" />
-                                    <h6 className="mt-2">Book</h6>
-                                    <p>Luv2Code</p>
-                                    <a className="btn main-color text-white" href="#">Reserve</a>
-                                </div>
-
-                            </div>
+                            <ReturnBook />
+                            <ReturnBook />
+                            <ReturnBook />
                         </div>
-
                     </div>
                     <button className="carousel-control-prev" type="button" data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
                         <span className="carousel-control-prev-icon" aria-hidden='true'></span>
@@ -63,14 +44,7 @@ export const Carousel = () => {
 
                 <div className="d-lg-none mt-3">
                     <div className="row d-flex justify-content-center align-items-center"></div>
-                    <div className="text-center">
-                        <img src={require('./../../Images/BooksImages/book-luv2code-1000.png')} width='151' height='233' alt="book" />
-                        <h6 className="mt-2">
-                            <b>Book</b>
-                        </h6>
-                        <p>Luv2Code</p>
-                        <a className="btn main-color text-white" href="#">Reserve</a>
-                    </div>
+                    <ReturnBook />
                 </div>
 
             </div>
