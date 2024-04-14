dd9b4ec6028f06fd486b4e7f73a526de22b89110
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-04-14 13:37:25 +0545

    addition of navigation bar

--- frontend/pustakalaya-frontend/src/App.tsx
+++ frontend/pustakalaya-frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Navbar/>
   );
 }
 
