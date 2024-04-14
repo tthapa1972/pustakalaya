c7467dd985cbf0503f8077b76b4f134c61957542
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-04-14 20:05:55 +0545

    Created Book Entity

---
+++ backend/pustakalaya/src/main/java/com/pustakalaya/pustakalaya/entity/Book.java
@@ -0,0 +1,36 @@
+package com.pustakalaya.pustakalaya.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private int copies;
+
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
+
+}
