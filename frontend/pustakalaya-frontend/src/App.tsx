188ea32edbbc0b43526d366b5f147d06fcbf4b3f
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-04-14 16:46:44 +0545

    Refactoring

--- frontend/pustakalaya-frontend/src/App.tsx
+++ frontend/pustakalaya-frontend/src/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
-import { Carousel } from './layouts/HomePage/Carousel';
-import { Heros } from './layouts/HomePage/Heros';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
 
-function App() {
+export const App = () =>  {
   return (
     <div>
       <Navbar/>
-      <ExploreTopBooks/>
-      <Carousel/>
-      <Heros/>
+      <HomePage/>
+      <Footer/>
     </div>
   );
 }
 
-export default App;
---
+++ frontend/pustakalaya-frontend/src/layouts/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+import { Carousel } from "./Components/Carousel";
+import { ExploreTopBooks } from "./Components/ExploreTopBooks";
+import { Heros } from "./Components/Heros";
+import { LibraryServices } from "./Components/LibraryServices";
+
+export const HomePage = () => {
+    return (
+        <div>
+            <ExploreTopBooks />
+            <Carousel />
+            <Heros />
+            <LibraryServices />
+        </div>
+    );
+}
