1dfddcf02c95c005c4edc7b2508430e73815e837
LAPTOP-1RT688NU\tarab <tara.thapa@securin.io>
2024-05-03 11:37:37 +0545

    Shelf - frontend

--- frontend/pustakalaya-frontend/src/App.tsx
+++ frontend/pustakalaya-frontend/src/App.tsx
@@ -8,9 +8,10 @@ import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import { BookCheckOutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 import { oktaConfig } from './lib/oktaConfig';
 import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js'
-import { LoginCallback, Security } from '@okta/okta-react';
+import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
 import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from './layouts/BookCheckoutPage/ReviewListPage/ReviewListPage';
+import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 
@@ -52,8 +53,8 @@ export const App = () => {
           }>
           </Route>
           <Route path='/login/callback' component={LoginCallback}>
-
           </Route>
+          <SecureRoute path='/shelf'><ShelfPage/></SecureRoute>
         </Switch>
       </div>
       </Security>
